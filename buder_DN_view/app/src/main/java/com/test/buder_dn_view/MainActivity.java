353d61ccaeb88f8a2a97d0de51061114060496cb
chenpeng02 <chenpeng02@qiyi.com>
2020-04-24 21:08:54 +0800

    初始化buder_DN_view

---
+++ buder_DN_view/app/src/main/java/com/test/buder_dn_view/MainActivity.java
@@ -0,0 +1,14 @@
+package com.test.buder_dn_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
