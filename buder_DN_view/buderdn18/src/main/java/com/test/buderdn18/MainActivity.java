bcd73182e1357f93c0146fd55f1fe134082da25e
chenpeng02 <chenpeng02@qiyi.com>
2020-04-25 15:56:29 +0800

    自定义菜单的背景view实现

--- buder_DN_view/buderdn18/src/main/java/com/test/buderdn18/MainActivity.java
+++ buder_DN_view/buderdn18/src/main/java/com/test/buderdn18/MainActivity.java
@@ -7,9 +7,25 @@ import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
 
+    MenuBgView menuBgView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        menuBgView = new MenuBgView(this);
+//        setContentView(menuBgView);
+
     }
+
+
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//
+//        menuBgView.setTouchY(event.getY(),0.8f);
+//
+//        return super.onTouchEvent(event);
+//
+//    }
 }
